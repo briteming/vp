a1119de58e9109f8a547eb7862b3e83085cc1cf8
vuvandinh123 <vuvandinh203@gmail.com>
2025-03-28 01:46:07 +0700

    add content: Lập Trình Hướng Đối Tượng (OOP) - Tư Duy và Ứng Dụng

--- src/containers/posts/ContentPost.tsx
+++ src/containers/posts/ContentPost.tsx
@@ -1,6 +1,17 @@
 "use client";
-import { MDXRemote } from "next-mdx-remote";
+import dynamic from "next/dynamic";
 import React from "react";
+
+const MDXRemote = dynamic(() => import("next-mdx-remote").then((mod) => mod.MDXRemote), {
+  ssr: false,
+  suspense: true,
+  loading: () => <div className="animate-pulse h-32 bg-gray-200 dark:bg-gray-800 rounded" />
+});
+
+// Preload the component
+if (typeof window !== 'undefined') {
+  import("next-mdx-remote");
+}
 
 export default function ContentPost({ source }: any) {
   return <div>{source && <MDXRemote {...source} />}</div>;
